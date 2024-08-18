5d38995ff62247db79b77b9b5dd01289f38d1a1b
manasdeo <rmnslogin@gmail.com>
2024-08-19 00:51:42 +0530

    search mobile view

--- Components/NavBar/page.tsx
+++ Components/NavBar/page.tsx
@@ -10,38 +10,57 @@ function NavBar() {
 
   return (
     <nav className={style.container}>
-      <div className={style.logo}>
-        <p className={style.title}>MaNaS Store</p>
-        <p className={style.slogan}>Dress to impress</p>
+      <div className={style.topBar}>
+        <div className={style.logo}>
+          <p className={style.title}>MaNaS Store</p>
+          <p className={style.slogan}>Dress to impress</p>
+        </div>
+        <form onSubmit={handleSubmit}>
+          <input
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            placeholder="Search for products, brands and more"
+            required
+            className={style.formInput}
+          />
+          <Image
+            src="/assets/icons/SearchIcon.svg"
+            alt="search icon"
+            width={30}
+            height={30}
+          />
+        </form>
+        <div className={style.cartProfile}>
+          <Image
+            src="/assets/icons/Profile.svg"
+            alt="search icon"
+            width={30}
+            height={30}
+          />
+          <Image
+            src="/assets/icons/Cart.svg"
+            alt="search icon"
+            width={30}
+            height={30}
+          />
+        </div>
       </div>
-      <form onSubmit={handleSubmit}>
-        <input
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-          placeholder="Search for products, brands and more"
-          required
-          className={style.formInput}
-        />
-        <Image
-          src="/assets/icons/SearchIcon.svg"
-          alt="search icon"
-          width={30}
-          height={30}
-        />
-      </form>
-      <div className={style.cartProfile}>
-        <Image
-          src="/assets/icons/Profile.svg"
-          alt="search icon"
-          width={30}
-          height={30}
-        />
-        <Image
-          src="/assets/icons/Cart.svg"
-          alt="search icon"
-          width={30}
-          height={30}
-        />
+      <div className={style.mobileForm}>
+        <form onSubmit={handleSubmit}>
+          <input
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            placeholder="Search for products, brands and more"
+            required
+            className={style.formInput}
+          />
+          <Image
+            src="/assets/icons/SearchIcon.svg"
+            alt="search icon"
+            width={30}
+            height={30}
+          />
+        </form>
       </div>
     </nav>
   );
